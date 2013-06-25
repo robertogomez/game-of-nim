694d46927862742acddb4442b1bb922040463dac
Roberto Gomez <hello@robertogomezjr.com>
2013-06-24 19:30:23 -0700

    - Updated CHANGES and README to 0.7.0 - Updated date in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------------*
  *	Title:			main.js											 *
  *	Author:			Roberto Gomez									 *
- *	Date:			6/17/13											 *
+ *	Date:			6/24/13											 *
  *	Description:	A robust and versatile take on the Game of Nim	 *
  *					using JS to manipulate DOM elements.			 *
  *-------------------------------------------------------------------*/
