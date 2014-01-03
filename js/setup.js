626bd5a96bddfd1e11c1a550db7f58241f73e19d
Roberto Gomez <mail@robertogomezjr.com>
2014-01-02 18:45:14 -0800

    Added setup.js
    Moved onclick handler for the play button from the HTML template to
    setup.js
    
    This further separates the structural content from the behavioral
    content

---
+++ js/setup.js
@@ -0,0 +1,9 @@
+/*----------------------------------------------------------------------------*
+ * Title:          setup.js                                                   *
+ * Author:         Roberto Gomez                                              *
+ * Date:           1/2/14                                                     *
+ * Description:    Registers the onclick events for the play and help buttons *
+ *----------------------------------------------------------------------------*/
+
+document.getElementById("play-button").onclick = Nim.init;
+
